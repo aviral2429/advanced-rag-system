68cf41df217612c7b0e9586f3916d4c980ddccee
Navya Kumar <411navya.kumar@gmail.com>
2026-02-16 17:06:19 +0530

    Initial clean commit

---
+++ vector_store.py
@@ -0,0 +1,5 @@
+from langchain_community.vectorstores import FAISS
+
+def create_vector_store(chunks, embeddings):
+    vectorstore = FAISS.from_documents(chunks, embeddings)
+    return vectorstore
